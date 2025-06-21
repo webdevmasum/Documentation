c5ece9090d43c5d7c807def900804fa35dc05e0e
webdevmasum <masumislam0018@gmail.com>
2025-06-21 19:46:45 +0600

    reqire once concept

--- Ostad/php app/todo list/important concept.php
+++ Ostad/php app/todo list/important concept.php
@@ -35,22 +35,50 @@ PHP File Include Statements
     Fatal error: require(): Failed opening required 'not_found.php'
 
 
+🟢 3. include_once
+    include_once 'config.php';
+
+
+🔹 Kaj:
+    File ta include korbe ekbar matro.
+    Jodi same file abar include kora hoy, skip kore.
+    Jodi file na thake → warning dibe (just like include).
+
+🔹 Example:
+    include_once 'config.php';
+    include_once 'config.php';
+
+
+🔸 Output:
+    Warning: include_once(config.php): failed to open stream...
+    Hello World!
+
+
+🔴 4. require_once 
+    require_once 'config.php';
+
+🔹 Kaj:
+    File ta ekbar matro include kore.
+    Jodi file na thake → fatal error dibe.
+    Best choice for database config, class file, etc.
+
+🔹 Example:
+    require_once 'config.php';
+    require_once 'config.php';
+
+🔸 Output:
+    Fatal error: require_once(): Failed opening required 'config.php'
+
+
+🔄 🔍 Summary Table:
+Statement	    File Missing → Error Type	Ekbar Include	    Use Case
+include	        ⚠️ Warning	                ✅ Multiple	      Optional file
+require	        ❌ Fatal Error	           ✅ Multiple	     Mandatory file
+include_once	⚠️ Warning	                ✅ Only Once	      Optional but avoid duplicate
+require_once	❌ Fatal Error  	           ✅ Only Once	     Most common for configs, classes
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
